3c071d5fe209fca3d9192c2af879c00373e6f601
sonalijain1999 <kcjain1970@gmail.com>
2021-11-23 16:11:46 +0530

    initial commit

--- assignment2/lib/main.dart
+++ assignment2/lib/main.dart
@@ -22,7 +22,7 @@ class _State extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:  AppBar(
-        title:  Text('Name here'),
+        title:  Text('Assignment2'),
       ),
       body:  Container(
           padding:  EdgeInsets.all(32.0),
